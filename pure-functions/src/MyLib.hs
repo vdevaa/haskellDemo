cc8f67717a0a85db67decbe26243f41b0d3d0ac6
vdevaa <vijay.deva@wsu.edu>
2026-01-27 15:07:15 -0800

    updated first demo to use our new testing framwork

--- pure-functions/src/MyLib.hs
+++ pure-functions/src/MyLib.hs
@@ -8,3 +8,11 @@ sub x y = x - y
 
 fac :: (Eq t, Num t) => t -> t
 fac x = if x == 0 then 1 else x * fac (x - 1)
+
+
+testAdd1 = if add 1 2 == 3
+    then "test passed"
+    else "test failed"
+
+runTests = do
+    putStrLn testAdd1
